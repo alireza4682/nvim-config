76ae95fb5364ffcdc82bad2c92f8781d8db8e820
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2024-01-20 10:03:45 +0330

    with leader w v and leader w w

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,5 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
+vim.keymap.set("n","<leader>wv",":vsplit<CR>",{})
+vim.keymap.set("n","<leader>ww","<C-w>w",{})
