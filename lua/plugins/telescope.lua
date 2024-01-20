4a31d7838a4f6b7afefcac96234da2f2482c4067
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2024-01-20 09:34:06 +0330

    float neo-tree and better find with leader f

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,9 +5,9 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-      vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>gg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>b', builtin.buffers, {})
     end
   },
   {
