533765e19624d6db4823a250dd15ceb27d5c11e3
alireza4682 <comolestyrusistantaneos@gmail.com>
2024-01-26 13:10:04 +0330

    with jump to new split

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,7 +5,7 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
 vim.g.mapleader = " "
-vim.keymap.set("n","<leader>wv",":vsplit<CR>",{})
+vim.keymap.set("n","<leader>wv",":vsplit<CR><C-w>w",{})
 vim.keymap.set("n","<leader>ww","<C-w>w",{})
 vim.diagnostic.config({signs=false,virtual_text=false,float={border='rounded'}})
 vim.keymap.set("n","<leader>i",":lua vim.diagnostic.open_float()<CR>,{}")
