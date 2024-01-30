3c1edddbd574b5bc651d2516e2e44a28d747d7b2
Alireza Arezoumandan <comolestyrusistantaneos@gmail.com>
2024-01-30 10:26:47 +0330

    new catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,23 +4,38 @@
         {
           color_overrides = {
             mocha = {
-              base= "#1c1917",
-              blue= "#22d3ee",
-              green= "#86efac",
-              flamingo= "#D6409F",
-              lavender= "#DE51A8",
-              pink= "#f9a8d4",
-              red= "#fda4af",
-              maroon= "#f87171",
-              mauve= "#D19DFF",
-              text= "#E8E2D9",
-              sky= "#7ee6fd",
-              yellow= "#fde68a",
-              rosewater= "#f4c2c2",
-              peach= "#fba8c4",
-              teal= "#4fd1c5"
-              }
-          },transparent_background=false,
+            rosewater = "#ffc9c9",
+            flamingo = "#ff9f9a",
+            pink = "#ffa9c9",
+            mauve = "#df95cf",
+            lavender = "#a990c9",
+            red = "#ff6960",
+            maroon = "#f98080",
+            peach = "#f9905f",
+            yellow = "#f9bd69",
+            green = "#b0d080",
+            teal = "#a0dfa0",
+            sky = "#a0d0c0",
+            sapphire = "#95b9d0",
+            blue = "#89a0e0",
+            text = "#e0d0b0",
+            subtext1 = "#d5c4a1",
+            subtext0 = "#bdae93",
+            overlay2 = "#928374",
+            overlay1 = "#7c6f64",
+            overlay0 = "#665c54",
+            surface2 = "#504844",
+            surface1 = "#3a3634",
+            surface0 = "#252525",
+            base = "#151515",
+            mantle = "#0e0e0e",
+            crust = "#080808",
+    },}
+  ,transparent_background=false,
+          integrations={
+            telescope=true,
+            mason = true,
+          }
         }
       )
     vim.cmd.colorscheme "catppuccin"
